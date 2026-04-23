5ff66c5145541b57bfdd05b8102c032e678a6491
Bukenya Daniel <dannyvardy45@gmail.com>
2026-04-23 14:18:53 +0300

    Updated backend from feature/token-refresh

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
+
 from apps.users.views import LoginView, RegisterView, UserListView, DashboardView, UserDeleteView
+
+from apps.users.views import LoginView, RegisterView, UserListView, DashboardView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
